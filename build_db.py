668ccb87267283ea02c9098bba6bbf84c063da4f
cade <cadeworm+git2@gmail.com>
2022-05-05 00:52:31 -0500

    Completed MongoDB potion of build_db.py.

--- build_db.py
+++ build_db.py
@@ -1,10 +1,10 @@
 import sqlite3 as sql
-from pymongo import MongoClient
+import pymongo as mongo
 import csv
 import os
 
 # dbPath stores the file location of sqlite3's db file.
-dbPath = './db/sql/recipe.db'
+dbPath = './db/sql/Recipe.db'
 
 # These variables store the path location of the raw data files in csv format.
 recipeMainPath = './raw_data/raw-data_recipe.csv'
@@ -14,7 +14,7 @@ recipeRatingsPath = './raw_data/raw-data_interaction.csv'
 # To make moving the resulting database easier, mongod is set to use the path ./db/mongo as its primary folder.
 
 # remove old sql db
-if (os.path.exists('./db/sql/recipe.db')):
+if (os.path.exists(dbPath)):
     print('Removing old SQLite database.')
     os.remove('./db/sql/recipe.db')
 
@@ -28,11 +28,11 @@ except Exception as e:
 else:
     print('Connection to SQLite successful. DB created.')
 
-# create tables/collections
+# create tables
 cursor = sqlConn.cursor()
 
 createRecipeMain = """CREATE TABLE RecipeMain (
-    recipe_id BLOB PRIMARY KEY,
+    recipe_id INTEGER PRIMARY KEY,
     recipe_name TEXT NOT NULL,
     avg_rate REAL NOT NULL,
     num_review INTEGER NOT NULL,
@@ -41,8 +41,8 @@ createRecipeMain = """CREATE TABLE RecipeMain (
     nutrition TEXT);"""
 
 createRecipeRatings = """CREATE TABLE RecipeRatings (
-    user_id BLOB,
-    recipe_id BLOB,
+    user_id INTEGER,
+    recipe_id INTEGER,
     rate INTEGER NOT NULL,
     last_modified TEXT NOT NULL,
     PRIMARY KEY (user_id, recipe_id),
@@ -58,10 +58,19 @@ cursor.commit()
 print('SQL tables created successfully.')
 
 # connect to mongo
+mongoConn = mongo.MongoClient()
 
-# remove old mongo db
+# remove old mongo collections, if they exist
+if ('Recipe' in mongoConn.list_database_names()):
+    if ('RecipeMain' in mongoConn.Recipe.list_collection_names()):
+        mongoConn.Recipe.RecipeMain.drop()
+    
+    if ('RecipeRatings' in mongoConn.Recipe.list_collection_names()):
+        mongoConn.Recipe.RecipeRatings.drop()
 
 # create new mongo db
+mongoRecipeMain = mongoConn.Recipe.RecipeMain
+mongoRecipeRatings = mongoConn.Recipe.RecipeRatings
 
 # populate RecipeMain table/collection
 with open(recipeMainPath, newline='') as csvFile:
@@ -69,11 +78,22 @@ with open(recipeMainPath, newline='') as csvFile:
     for line in dictReader:
         # add line to sql db
         sqlInsert = f"""INSERT INTO RecipeMain (recipe_id, recipe_name, avg_rate, num_review, ingredients, directions, nutrition)
-            VALUES ({line['recipe_id']}, {line['recipe_name']}, {line['aver_rate']}, {line['review_nums']}, 
+            VALUES ({int(line['recipe_id'])}, {line['recipe_name']}, {float(line['aver_rate'])}, {int(line['review_nums'])}, 
                 {line['ingredients']}, {line['cooking_directions']}, {line['nutritions']});"""
         cursor.execute(sqlInsert)
         sqlConn.commit()
+
         # add line to mongo db
+        mongoInsert = {
+            'recipe_id': int(line['recipe_id']),
+            'recipe_name': line['recipe_name'],
+            'avg_rate': float(line['aver_rate']),
+            'num_review': int(line['review_nums']),
+            'ingredients': line['ingredients'],
+            'directions': line['cooking_directions'],
+            'nutrition': line['nutritions']
+        }
+        mongoRecipeMain.insert_one(mongoInsert)
 
 print('RecipeMain SQL table and Mongo collection populated successfully.')
     
@@ -83,11 +103,21 @@ with open(recipeRatingsPath, newline='') as csvFile:
     for line in dictReader:
         # add line to sql db
         sqlInsert = f"""INSERT INTO RecipeRatings (user_id, recipe_id, rate, last_modified)
-            VALUES ({line['user_id']}, {line['recipe_id']}, {line['rating']}, {line['dateLastModified']});"""
+            VALUES ({int(line['user_id'])}, {int(line['recipe_id'])}, {int(line['rating'])}, {line['dateLastModified']});"""
         cursor.execute(sqlInsert)
         sqlConn.commit()
-        # add line to mongo db
 
+        # add line to mongo db
+        mongoInsert = {
+            'user_id': int(line['user_id']),
+            'recipe_id': int(line['recipe_id']),
+            'rating': int(line['rating']),
+            'last_modified': line['dateLastModified']
+        }
+        mongoRecipeMain.insert_one(mongoInsert)
+
+# close connections after finished
 cursor.close()
 sqlConn.close()
+mongoConn.close()
 print('RecipeRatings SQL table and Mongo collection populated successfully.')
---
+++ query_db.cs
@@ -0,0 +1,8 @@
+class RecipeMDB {
+    /*
+        TODO: a lot. 
+        1) make methods to easily query each database and return results.
+        2) write tests for the databases, measure performance in ms.
+        3) write code for gui queries. (different file?)
+    */
+}
