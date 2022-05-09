dc0223c061fd3ebe91bc58cd7e3ee35596adc71d
cade <cadeworm+git2@gmail.com>
2022-05-09 12:24:28 -0500

    Added some changes to make VSCode compatible with .NET solutions and C# projects.

---
+++ RecipeMDB/query_db.cs
@@ -0,0 +1,37 @@
+namespace RecipeMDB {
+    using System;
+    using System.Data.SQLite;
+    /*
+        TODO: a lot. 
+        1) make methods to easily query each database and return results.
+        2) write tests for the databases, measure performance in ms.
+        3) write code for gui queries. (different file?)
+    */
+    public class MongoInst {
+
+    }
+
+    public class SQLiteInst {
+
+    }
+
+
+    internal class Tests {
+        static string Test1() {
+            return string.Empty;
+        }
+
+        static string Test2() {
+            return string.Empty;
+        }
+        static string Test3() {
+            return string.Empty;
+        }
+        static string Test4() {
+            return string.Empty;
+        }
+        static string Test5() {
+            return string.Empty;
+        }
+    }
+}
--- query_db.cs
+++
@@ -1,8 +0,0 @@
-class RecipeMDB {
-    /*
-        TODO: a lot. 
-        1) make methods to easily query each database and return results.
-        2) write tests for the databases, measure performance in ms.
-        3) write code for gui queries. (different file?)
-    */
-}
